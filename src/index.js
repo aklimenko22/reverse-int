9c2a881b9644302b5083f7fa78ee9cb4fef56941
aklimenko22 <nst22@yandex.ru>
2020-10-10 21:45:00 +0300

    fix: implement correct solution with '-'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function reverse (n) {
-    let str = String(n);
-    let result = '';
-    for (i = str.length - 1; i >= 0; i--) {
-      result += str[i];
+        let str = String(n);
+        let result = '';
+        for (i = str.length - 1; i >= 0; i--) {
+          result += str[i];
+        }
+        if (result[str.length - 1] == '-') {
+        return result.slice(0, result.length - 1);
+        }
+        return result;
     }
-    if (result[str.length - 1] == '-') {
-    let result = result.substring(0, result.length — 1);
-    }
-    return result;
-}
