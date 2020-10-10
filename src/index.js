96d67550a539c6fd70582905fea36aa681400dbc
aklimenko22 <nst22@yandex.ru>
2020-10-10 20:09:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let reverse = "";
+    for (const i = n.length - 1; i >= 0; i--) {
+        reverse += n[i];
+    }
+    return reverse;
+}
+
+console.log(reverse (n)); 
 }
